b02d8a8ab4bf36e3d5e608787ab675ed948cc157
Alessandro Maggio <alex.may2002@gmail.com>
2025-10-22 15:13:56 +0200

    added and example of the config.sh file in config_example.sh

---
+++ config_example.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# global configuration for the network
+# port where the various devices can be accessed
+PORT_TEL=""
+PORT_STD=""
+
+#ip and usernames of the various devices
+IP_SERVER=""
+USER_SERVER=""
+
+IP_COMP=""
+USER_COMP=""
+
+IP_TEL=""
+USER_TEL=""
+
+#home path of the various devices
+HOME_TEL=""
+REMOTE_DIR_TEL=""
+
+HOME_SERVER=""
+
+HOME_COMP=""
+
+LOCAL_DIR_SERVER=""
+LOCAL_DIR_TEL=""
